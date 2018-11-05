2e731ee4ba44c8abfc0d75ca494660ad596ec43a
jerry0317 <yanjieyu1999@hotmail.com>
2018-11-05 11:00:58 -0800

    Add a test API
    I built an API entirely on python with Flask. The API locates at api.html using POST method.

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from flask import Flask, url_for, render_template, request
 import os
 import gitGQL
+import json
 app = Flask(__name__)
 
 @app.route("/")
 def render_index():
     lastC = gitGQL.recentCommits()
     return render_template('index.html', lastC = lastC)
+
+@app.route("/api", methods=['POST'])
+def parse_request():
+    name = request.form['name']
+    if name == "":
+        msg = "Please provide your name..."
+    else:
+        msg = "Hi, {}!".format(name)
+    rt = {
+        "message": msg
+    }
+    rt = json.dumps(rt)
+    return rt
 
 if __name__ == "__main__":
     app.run(port=5000)
